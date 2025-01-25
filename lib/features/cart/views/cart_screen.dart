77c47f4383bcf4440bd2e08ff880784a78b6079b
sachin supunthaka <sachinsupunthaka96@gmail.com>
2025-01-25 12:40:12 +0530

    chor: cart screen with tabs

--- lib/features/cart/views/cart_screen.dart
+++ lib/features/cart/views/cart_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class CartScreen extends StatefulHookConsumerWidget {
+  static const routeName = '/cart';
+
   const CartScreen({super.key});
 
   @override
@@ -11,6 +13,33 @@ class CartScreen extends StatefulHookConsumerWidget {
 class _CartScreenState extends ConsumerState<CartScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Cart Page'),
+          bottom: TabBar(
+            labelColor: Colors.deepOrange.shade600,
+            unselectedLabelColor: Colors.deepOrange.shade900,
+            tabs: [
+              Tab(
+                icon: Icon(Icons.shopping_cart),
+              ),
+              Tab(
+                icon: Icon(Icons.credit_card),
+              ),
+              Tab(
+                icon: Icon(Icons.receipt),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(children: [
+          Text('Credit Card'),
+          Text('Shopping Card'),
+          Text('Receipt')
+        ]),
+      ),
+    );
   }
 }
