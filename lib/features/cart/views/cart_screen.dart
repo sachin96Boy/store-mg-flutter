8c9c79a26fc5e2ebdf7fb90e3f5c56c19bf7ccc0
sachin supunthaka <sachinsupunthaka96@gmail.com>
2025-01-28 12:44:46 +0530

    chor: clear unused imports

--- lib/features/cart/views/cart_screen.dart
+++ lib/features/cart/views/cart_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:store_mg_fl/common/components/product_item.dart';
-import 'package:store_mg_fl/features/cart/models/cart_model.dart';
 import 'package:store_mg_fl/features/cart/providers/cart_provider.dart';
 import 'package:store_mg_fl/features/products/models/product_model.dart';
 
